b1d50e73a1a8b44e0cace42533c7255fe952f625
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-04 15:51:38 -0800

    add styling and round score counter
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail>

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,19 @@ Game.prototype.addPlayer = function(playerObject){
 Game.prototype.findPlayer = function(buttonClasslist){
   for( let i = 0; i < this.players.length; i++){
     if(buttonClasslist.includes(this.players[i].id)){
-      return this.players[i].id;
+      return this.players[i];
     }
   }
 }
 
 function Player (name){
   this.name = name;
+  this.score = 0;
+}
+
+Player.prototype.newScore = function(roll){
+  this.score += roll;
+  return this.score;
 }
 // User Logic
 
@@ -28,7 +34,7 @@ function gameStart(players_array){
     if(players_array.length === 2){
       $(".player-1").text(players_array[0].name);
       $(".player-2").text(players_array[1].name);
-      switchUser();
+      $("#2").hide();
       $("#form").hide();
       $("#game").show();
     }
@@ -36,19 +42,21 @@ function gameStart(players_array){
 }
 
 function switchUser (currentPlayer) {
-  if (currentPlayer === 0) {
-    $("#1").fadeOut();
-    $("#2").fadeIn();
+  console.log(currentPlayer.id);
+  currentPlayer.score = 0;
+  if (currentPlayer.id === 0) {
+    $("#1").hide();
+    $("#2").show();
   } else {
-    $("#2").fadeOut();
-    $("#1").fadeIn();
+    $("#2").hide();
+    $("#1").show();
   }
 }
 
 function addClickEvent(game){
-
   $(".buttons").on('click', 'button.roll', function(){
     let currentPlayer = game.findPlayer(this.classList[1]);
+    console.log(currentPlayer);
     roll(currentPlayer);
   }); 
 }
@@ -58,9 +66,11 @@ function roll(currentPlayer){
   let roll = Math.floor((Math.random() * 6) + 1);
   dice.innerHTML = roll;
   if (roll === 1) {
+    $("#score").text("Round Total: "+ 0);
     switchUser(currentPlayer);
   } else if (roll != 1) {
-
+    currentPlayer.newScore(roll);
+    $("#score").text("Round Total: "+ currentPlayer.score);
   }
 
 }
